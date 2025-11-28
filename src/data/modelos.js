debee5298ab2c2050de7a27c9f8701f094d8ab53
Carlitos <drusskinds@gmail.com>
2025-11-27 21:03:27 -0500

    Primer commit

---
+++ src/data/modelos.js
@@ -0,0 +1,148 @@
+export const modelos = [
+    {
+        id: "go",
+        nombre: "GO",
+        imagen: "/imagenes/imagenes-modelos/modelo1.png",
+        cantidad: 5,
+        electrico: true,
+        descripcion: [
+            "HATCHBACK 100% ELÉCTRICO",
+            "Batería Blade",
+            "Ultra-Safe",
+            "30 minutos",
+            "30%-80% de carga rápida",
+            "Plataforma-E 3.0",
+            "Destacando la inteligencia, eficiencia seguridad",
+            "SUAQUITA GO",
+            "EL SUAQUITA GO es el miembro más pequeño y dinámico de la línea del Ocean Serie, e interpreta la fuerte vitalidad de la “Ocean Aesthetics” con su forma nítida y su estilo dinámico.",
+            "Diseño Ocean Aesthetics",
+            "El SUAQUITA GO hereda la filosofía de diseño Estética Oceánica. Bajo la dirección del Director Global de Diseño de BYD, Wolfgang Egger, se enfatiza una apariencia audaz y agresiva. Las líneas alargadas del cuerpo eliminan restricciones visuales y la línea dinámica única de la cintura asciende desde la puerta delantera hasta el pilar C. Las líneas afiladas y la superficie curva de máxima tensión muestran su postura elegante.",
+            "Cabina Ocean Chic",
+            "Cabina Inteligente con la característica pantalla giratoria de BYD de 10.1 pulgadas con conexión a Apple CarPlay y Android CarPlay. Comando de voz Hi BYD que responde a tus órdenes sin soltar las manos del volante. Versión BYD DOLPHIN MINI PLUS con carga inalámbrica para smartphones de 50w",
+            "El Seagull integra seguridad y bajo consumo de energía",
+            "El SUAQUITA GO es el primer vehículo totalmente eléctrico de BYD que ofrece un nuevo nivel de eficiencia, mejora significativamente el nivel de seguridad, permite un mayor espacio interior y cuenta con funciones inteligentes. El eficiente sistema de propulsión 8 en 1 reduce eficazmente el consumo de energía del vehículo. El sistema de bomba de calor mejora la resistencia en invierno entre un 10% y un 12%, y el motor de alambre plano tiene un rango de funcionamiento eficiente más amplio, reduciendo el consumo de energía.",
+            "Plataforma-E 3.0",
+            "El BYD SEAGULL cuenta con la batería blade súper segura y hereda los atributos de seguridad de la plataforma electrónica 3.0, brindando a los usuarios una estructura de carrocería segura exclusiva de los vehículos eléctricos.",
+            "BYD SEAGULL",
+        ],
+        galeria: [
+            "/imagenes/imagenes-modelos/modelo-go/1-banner.jpg",
+            "/imagenes/imagenes-modelos/modelo-go/2-banner.jpg",
+            "/imagenes/imagenes-modelos/modelo-go/3-banner.jpg",
+            "/imagenes/imagenes-modelos/modelo-go/4-banner.jpg",
+            "/imagenes/imagenes-modelos/modelo-go/5-banner.jpg",
+            "/imagenes/imagenes-modelos/modelo-go/6-banner.jpg",
+            "/imagenes/imagenes-modelos/modelo-go/7-banner.jpg",
+            "/imagenes/imagenes-modelos/modelo-go/8-banner.jpg",
+        ],
+    },
+
+    {
+        id: "lumina",
+        nombre: "LUMINA",
+        imagen: "/imagenes/imagenes-modelos/modelo2.png",
+        cantidad: 6,
+        electrico: true,
+        descripcion: [
+            "SEDÁN 100% ELÉCTRICO DINÁMICO E INTELIGENTE",
+            "3.8 s",
+            "Aceleración de 0-100 km/h",
+            "520km",
+            "Autonomía ultralarga (WLTC)",
+            "CTB",
+            "Tecnología Cell to Body",
+            "Cara X Oceánica",
+            "El impresionante BYD SEAL es una verdadera obra maestra del diseño inspirado en el océano, con un frente en forma de X que emana deportividad y estilo. Se enorgullece de recibir el codiciado iF Design Award, lo que consolida su diseño e innovación superiores.",
+            "Tecnología CTB (Cell to Body)",
+            "BYD SEAL es el primer modelo equipado con tecnología CTB desarrollada internamente en BYD. La tecnología de vanguardia integra la Batería Blade a la perfección en la carrocería del automóvil, formando una fuerte estructura de sándwich que puede lograr una notable rigidez torsional de más de 40,500 Nm/°, equivalente a los automóviles de lujo. Con la tecnología CTB, la Batería Blade es más que una fuente de energía. También es un componente estructural capaz de soportar una fuerza significativa.",
+            "Batería Blade BYD",
+            "BYD ha sido un referente en la industria de las baterías durante más de 28 años. Nuestra última e innovadora Batería Blade ha superado una serie de pruebas extremas en condiciones rigurosas, lo que la convierte en una de las baterías más seguras del mundo.",
+            "Cabina Inteligente y Cómoda",
+            "La cómoda cabina construida para BYD SEAL basada en una Estética Oceánica, te permite sentirte cómodo y relajado así como experimentar la atmósfera general llena de vitalidad. Tiene un excelente diseño ergonómico y un acabado exquisito que interpretan la connotación de consideración y alta calidad.",
+            "SUAQUITA SEAL",
+        ],
+        galeria: [
+            "/imagenes/imagenes-modelos/modelo-lumina/1-PC.jpg",
+            "/imagenes/imagenes-modelos/modelo-lumina/2-PC.jpg",
+            "/imagenes/imagenes-modelos/modelo-lumina/3-PC.jpg",
+            "/imagenes/imagenes-modelos/modelo-lumina/4-PC.jpg",
+            "/imagenes/imagenes-modelos/modelo-lumina/5-PC.jpg",
+            "/imagenes/imagenes-modelos/modelo-lumina/6-PC.jpg",
+            "/imagenes/imagenes-modelos/modelo-lumina/7-PC.jpg",
+        ],
+    },
+    {
+        id: "aura",
+        nombre: "AURA",
+        imagen: "/imagenes/imagenes-modelos/modelo4.png",
+        cantidad: 8,
+        electrico: true,
+        descripcion: [
+            "SUV 100% ELÉCTRICO DE 7 ASIENTOS", //0
+            "635 km", //1
+            "Autonomía (CLTC)", //2
+            "4.4s", //3
+            "Aceleración de 0-100km/h", //4
+            "AWD", //5
+            "Tracción eléctrica en las cuatro rueda", //6
+            "SUV Poderoso y Flexible para Cualquiera Aventuras", //7
+            "BYD TANG, con capacidad para siete pasajeros, tracción en las cuatro ruedas, y los más altos estándares de equipamiento. Los asientos traseros son plegables y divididos, se pueden convertir en un espacio grande para garantizar que puedas llevar todo tu equipo contigo a la aventura.", //8
+            "Armonía entre Estilo y Eficiencia", //9
+            "Los dragones simbolizan poderes potentes y auspiciosos. Wolfgang Egger, Director de Diseño Global de BYD, ha dirigido un equipo de diseño de clase mundial para inculcar estas características en el diseño de BYD TANG, reuniendo una excelente artesanía, dinamismo y elegancia.", //10
+            "Una Conducción de Espectáculo", //11
+            "BYD TANG está diseñado por el maestro de diseño de clase mundial Michele Paganetti. El espacioso de lujo, los materiales de alta calidad y la experiencia inmersiva hacen que cada viaje sea placentero y cómodo.", //12
+            "La seguridad es Nuestro Estándar", //13
+            "Al conducir BYD TANG, queremos que los conductores y pasajeros se sumerjan completamente en la experiencia y con total tranquilidad. BYD TANG está equipado con las últimas generaciones de asistencia al conductor y pasajeros. Nuestro objetivo es asegurar que tu viaje sea cómodo, relajante y, sobre todo, seguro.", //14
+            "BYD TANG", //15
+        ],
+        galeria: [
+            "/imagenes/imagenes-modelos/modelo-aura/1-pc.jpg",
+            "/imagenes/imagenes-modelos/modelo-aura/2-pc.jpg",
+            "/imagenes/imagenes-modelos/modelo-aura/3-pc.jpg",
+            "/imagenes/imagenes-modelos/modelo-aura/4-pc.jpg",
+            "/imagenes/imagenes-modelos/modelo-aura/5-pc.jpg",
+            "/imagenes/imagenes-modelos/modelo-aura/6-pc.jpg",
+            "/imagenes/imagenes-modelos/modelo-aura/7-pc.jpg",
+        ],
+    },
+    {
+        id: "amazonia",
+        nombre: "AMAZONIA",
+        imagen: "/imagenes/imagenes-modelos/modelo5.png",
+        cantidad: 9,
+        electrico: true,
+        descripcion: [
+            "DESPIERTA LA ENERGÍA",
+            "401 km",
+            "Autonomía EV (CLTC)",
+            "7.9s",
+            "Aceleración de 0 a 100 km/h",
+            "e-Platform 3.0",
+            "Inteligencia, eficiencia, seguridad y estética",
+            "Atractivo, seguro y fácil de operar",
+            "Este SUV combina frescura, deportividad, espacio y encanto, creando un vehículo que no solo es atractivo a la vista, sino también altamente funcional e inteligente.",
+            "Diseño de Dinastía",
+            "El diseño evolucionado del BYD Yuan UP, está inspirado en el concepto Familia DYNASTY, que fusiona magistralmente lo tradicional y lo contemporáneo, reflejado en las líneas limpias y sobrias en el exterior, como en algunos elementos interiores.",
+            "Interior",
+            "El interior del Yuan UP ofrece comodidad y estilo con tapicería premium.",
+            "e-Platform 3.0",
+            "Gracias a sus extraordinarias ventajas en inteligencia, eficiencia, seguridad y estética, la e-Platform 3.0 pretende mejorar la seguridad y la autonomía de los Vehículos de Nueva Energía (NEV) a bajas temperaturas, así como la experiencia de conducción inteligente, con el fin de crear nuevos vehículos eléctricos inteligentes más eficientes y seguros.",
+            "Súper seguro",
+            "El Yuan UP está equipado con avanzados sistemas de seguridad para garantizar la protección de todos los ocupantes. Cuenta con 2 airbags delanteros, 2 laterales y 2 de cortina. Los cinturones de seguridad delanteros y traseros tienen pretensionadores y limitadores de fuerza, además de alarmas para garantizar su uso.",
+            "DIPLOT Sistemas inteligentes",
+            "Entre los sistemas de asistencia destacan la cámara de reversa con imagen panorámica 360°, ABS con EBD, control electrónico de estabilidad, así como sistemas exclusivos de BYD cómo son el IPB (Intergrated Parking Brake) , CST. Adicional a esto, cuenta con los sistemas TCS (Control de tracción), HDC (Control de Descenso de Pendientes), control crucero y sensores de parqueo delanteros y traseros. Además, dispone de un sistema de monitoreo de presión de las llantas, seguros para niños, bloqueo automático de puertas mediante sensor de velocidad e inmovilizador con sistema antirrobo.",
+            "BYD YUAN UP",
+        ],
+        galeria: [
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-01.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-02.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-03.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-04.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-05.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-06.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-07.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-08.jpg",
+            "/imagenes/imagenes-modelos/modelo-amazonia/PC-09.jpg",
+        ],
+    }
+]
